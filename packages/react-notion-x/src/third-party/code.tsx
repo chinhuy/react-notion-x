3dfb122d2345bd3ec4082d2f9b44b75c19928776
Uy Pham <gentleuyphamit@gmail.com>
2023-02-01 19:24:48 +0700

    support mermaid code block

--- packages/react-notion-x/src/third-party/code.tsx
+++ packages/react-notion-x/src/third-party/code.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import copyToClipboard from 'clipboard-copy'
+import mermaid from 'mermaid'
 import { CodeBlock } from 'notion-types'
 import { getBlockTitle } from 'notion-utils'
 import { highlightElement } from 'prismjs'
@@ -64,29 +65,84 @@ export const Code: React.FC<{
     </div>
   )
 
-  return (
-    <>
-      <pre className={cs('notion-code', className)}>
-        <div className='notion-code-copy'>
-          {copyButton}
+  if (language === 'mermaid') {
+    mermaid.initialize({
+      startOnLoad: true,
+      theme: 'default',
+      securityLevel: 'loose',
+      themeCSS: `
+          g.classGroup rect {
+            fill: #282a36;
+            stroke: #6272a4;
+          }
+          g.classGroup text {
+            fill: #f8f8f2;
+          }
+          g.classGroup line {
+            stroke: #f8f8f2;
+            stroke-width: 0.5;
+          }
+          .classLabel .box {
+            stroke: #21222c;
+            stroke-width: 3;
+            fill: #21222c;
+            opacity: 1;
+          }
+          .classLabel .label {
+            fill: #f1fa8c;
+          }
+          .relation {
+            stroke: #ff79c6;
+            stroke-width: 1;
+          }
+          #compositionStart, #compositionEnd {
+            fill: #bd93f9;
+            stroke: #bd93f9;
+            stroke-width: 1;
+          }
+          #aggregationEnd, #aggregationStart {
+            fill: #21222c;
+            stroke: #50fa7b;
+            stroke-width: 1;
+          }
+          #dependencyStart, #dependencyEnd {
+            fill: #00bcd4;
+            stroke: #00bcd4;
+            stroke-width: 1;
+          }
+          #extensionStart, #extensionEnd {
+            fill: #f8f8f2;
+            stroke: #f8f8f2;
+            stroke-width: 1;
+          }`,
+      fontFamily: 'Fira Code'
+    })
+    return <div className='mermaid'>{content}</div>
+  } else {
+    return (
+      <>
+        <pre className={cs('notion-code', className)}>
+          <div className='notion-code-copy'>
+            {copyButton}
 
-          {isCopied && (
-            <div className='notion-code-copy-tooltip'>
-              <div>{isCopied ? 'Copied' : 'Copy'}</div>
-            </div>
-          )}
-        </div>
+            {isCopied && (
+              <div className='notion-code-copy-tooltip'>
+                <div>{isCopied ? 'Copied' : 'Copy'}</div>
+              </div>
+            )}
+          </div>
 
-        <code className={`language-${language}`} ref={codeRef}>
-          {content}
-        </code>
-      </pre>
+          <code className={`language-${language}`} ref={codeRef}>
+            {content}
+          </code>
+        </pre>
 
-      {caption && (
-        <figcaption className='notion-asset-caption'>
-          <Text value={caption} block={block} />
-        </figcaption>
-      )}
-    </>
-  )
+        {caption && (
+          <figcaption className='notion-asset-caption'>
+            <Text value={caption} block={block} />
+          </figcaption>
+        )}
+      </>
+    )
+  }
 }
